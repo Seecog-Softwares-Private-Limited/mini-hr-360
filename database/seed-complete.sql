b59e8d95f3e81f6deba9a24957191a10fb909a39
mukeshkumar906 <mukesh@seecogsoftwares.com>
2026-01-09 13:14:17 +0530

    fixed

--- database/seed-complete.sql
+++
@@ -1,1 +0,0 @@
-use mini_hr_360;
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -3,17 +3,15 @@ import { loginUser, logoutUser } from '../controllers/user/login.js';
 import register from '../controllers/user/register.js';
 import { verifyUser } from '../middleware/authMiddleware.js';
 import { refresh } from '../controllers/user/refreshToken.js';
-import { forgotPassword } from '../controllers/user/forgotPassword.js';
-import { resetPassword } from '../controllers/user/resetPassword.js';
+import { checkEmail } from '../controllers/user/checkEmail.js';
 
 
 const router = express.Router();
 
 router.post('/register', register);
+router.get('/check-email', checkEmail);
 router.post('/login', loginUser);
 router.post("/refresh", refresh);
 router.post('/logout', verifyUser, logoutUser);
-router.post('/forgot-password', forgotPassword);
-router.post('/reset-password', resetPassword);
 
 export default router;
